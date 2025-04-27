38534fbf9ce0ff8727207b199cbf8c6c8f1c137c
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-04-27 09:34:35 +0200

    prep exc

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -1,14 +1,43 @@
 /**
  * Exercise 3: Create an HTTP web server
  */
-
-const http = require('http');
+const fs = require('fs').promises
+const path = require('path') 
+const http = require('http')
 
 //create a server
-let server = http.createServer(function (req, res) {
+let server = http.createServer(async function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
-});
+  try {
+    if (req.url === '/') {
+      const data = await fs.readFile(path.join(__dirname, 'index.html'))
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      })
+      res.end(data)
+    } else if (req.url === '/index.js') {
+      const data = await fs.readFile(path.join(__dirname, 'index.js'))
+      res.writeHead(200, {
+        'Content-Type': 'application/javascript',
+      })
+      res.end(data)
+    } else {
+      res.writeHead(404, {
+        'Content-Type': 'text/plain',
+      })
+      res.end('404 not found')
+    }
+  } catch (err) {
+    res.writeHead(500, {
+      'Content-Type': 'text/plain',
+    })
+    res.end('server error')
+    console.error(err)
+  }
+  //   res.write('Hello World!') // Sends a response back to the client
+  //   res.end() // Ends the response
+})
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log('server is running (listen on port 3000)')
+}) // The server starts to listen on port 3000
