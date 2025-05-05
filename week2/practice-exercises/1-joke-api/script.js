28734c9b46e0e28e01d30f2ba601fe7398da0c77
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-05-05 21:13:31 +0200

    smth

--- week2/practice-exercises/1-joke-api/script.js
+++ week2/practice-exercises/1-joke-api/script.js
@@ -1,18 +1,25 @@
 /**
  * 1. Chuck Norris programs do not accept input
- * 
+ *
  * `GET` a random joke inside the function, using the API: http://www.icndb.com/api/
- * (use `node-fetch`) and print it to the console. 
+ * (use `node-fetch`) and print it to the console.
  * Make use of `async/await` and `try/catch`
- * 
+ *
  * Hints
  * - To install node dependencies you should first initialize npm
  * - Print the entire response to the console to see how it is structured.
  */
+import fetch from 'node-fetch'
 
-function printChuckNorrisJoke() {
-  // YOUR CODE GOES IN HERE
-
+async function printChuckNorrisJoke() {
+  try {
+    const response = await fetch('https://api.chucknorris.io/jokes/random')
+    // const body = await response.text()
+    const data = await response.json()
+    // console.log(response);
+    console.log(data.value)
+  } catch (err) {
+    console.error(err)
+  }
 }
-
-printChuckNorrisJoke();
+printChuckNorrisJoke()
