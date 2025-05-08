380d8bdc9bea841cd0b5353544a887f6e903794b
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-05-08 12:51:38 +0200

    simple commit

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 // TODO Use below import statement for importing middlewares from users.js for your routes
 // TODO import { ....... } from "./users.js";
+
 
 let app = express();
 
 app.use(express.json());
 // TODO: Create routes here, e.g. app.post("/register", .......)
+app.post('/register', async (req, res) => {
+const { username, password } = req.body
+
+})
 
 // Serve the front-end application from the `client` folder
 app.use(express.static('client'));
